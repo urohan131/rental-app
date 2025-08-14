070a946474511f10e4cb8dbeea659b5512c63e20
Rohan Kumar <urohan131@gmail.com>
2025-08-14 19:19:19 +0530

    Setting up of the tenant dashboard

--- src/app/(nondashboard)/layout.tsx
+++ src/app/(nondashboard)/layout.tsx
@@ -2,9 +2,39 @@
 
 import Navbar from "@/components/Navbar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import React from "react";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const { data: authUser, isLoading: authLoading } = useGetAuthUserQuery();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (!authLoading && authUser) {
+      const userRole = authUser.userRole?.toLowerCase();
+      if (
+        (userRole === "manager" && pathname.startsWith("/search")) ||
+        (userRole === "manager" && pathname === "/")
+      ) {
+        router.push("/managers/properties", { scroll: false });
+      }
+    }
+  }, [authUser, authLoading, router, pathname]);
+
+  if (authLoading) {
+    return <>Loading...</>;
+  }
+
+  const isManagerOnNonManagerPage =
+    authUser?.userRole?.toLowerCase() === "manager" &&
+    (pathname.startsWith("/search") || pathname === "/");
+
+  if (isManagerOnNonManagerPage) {
+    return <>Loading...</>;
+  }
+
   return (
     <div className="h-full w-full">
       <Navbar />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers> 
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
