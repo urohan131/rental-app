7bedc2f0897cbb11c1146f4151a78e6c87502c4b
Rohan Kumar <urohan131@gmail.com>
2025-08-17 00:57:22 +0530

    final adjustments

--- client/next.config.ts
+++ client/next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol:"https",
+        hostname:"*.amazon.com",
+        port:"",
+        pathname:"/**",
+      }
     ],
   },
 };
